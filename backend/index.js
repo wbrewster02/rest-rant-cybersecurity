c766c2ceb6b45e18226e7203600ca7a72e0b872f
wbrewster02 <wbrewster02@gmail.com>
2023-08-06 14:18:17 -0400

    session authentication

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,20 @@ const cors = require('cors')
 const app = express();
 
 // Express Settings
-app.use(cors())
+const cookieSession = require('cookie-session')
+
+// Express Settings
+app.use(cookieSession({
+    name: 'session',
+    keys: [ process.env.SESSION_SECRET ],
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}
+))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -17,7 +30,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
-app.use('/authentication', require('/controllers/authentication'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
